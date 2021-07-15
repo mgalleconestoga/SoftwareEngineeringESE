b1c07cd8d6e6a813d49efa7db450e68470ec7045
mgalleconestoga <mgalle@conestogac.on.ca>
2021-07-15 10:54:55 -0700

    updated content for Back End Development 7 lecture

--- php/member.php
+++ php/member.php
@@ -1,28 +1,73 @@
-<?php
-// member.php contains members only content
-session_start(); // Required for every page that uses session_start
+<html>
+<head><title>Members only page</title></head>
+<body> 
 
-// Ensure that users not logged in do not have access to this page
-if (isset($_SESSION['username'])) {
+    <h1>Members only page</h1> 
+    <p>Exclusive content for members only</p>
+    <?php
+        session_start();
+        if(isset($_SESSION['username']) && isset($_SESSION['password'])) {
+            // User is authenticated
+            echo 'Welcome ' . $_SESSION['username'];
+            echo '<p>Members ONLY content </p>';
+            echo '<p>Please click <a href=\'logout.php\'>here</a> to log out</p>';
 
-  // Page content goes here
-  echo 'Welcome, ' . $_SESSION['username'] . '!<br />';
-  echo 'This is where our members only content will go';
+            require '../HTML/elevatorNetworkForm.html';          // Insert to database form 
+
+            // Get data from database
+            // Connect to $db
+            $db = new PDO(
+                'mysql:host=127.0.0.1;dbname=elevator',     // Database name
+                'michaelG',                                 // username
+                'myPassword'                                // Password
+            );
+            $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+            $submitted = !empty($_POST);                    // You may want to prevent form resubmission           
+            if($submitted) {
+                //echo "You submitted the form<br/>"; 
+                // INSERT shown here (you can use a different string query for DELETE and UPDATE)
+                $query = 'INSERT INTO elevatorNetwork (date, time, status, currentFloor, requestedFloor, otherInfo) 
+                         VALUES(:date, :time, :status, :currentFloor, :requestedFloor, :otherInfo)';
+                
+                $statement = $db->prepare($query); 
+                $curr_date_query = $db->query('SELECT CURRENT_DATE()'); 
+                $curr_date = $curr_date_query->fetch(PDO::FETCH_ASSOC); 
+                $curr_date_time = $db->query('SELECT CURRENT_TIME()'); 
+                $curr_time = $curr_date_time->fetch(PDO::FETCH_ASSOC); 
+                $status = $_POST['status'];
+                $currentFloor = $_POST['currentFloor'];
+                $requestedFloor = $_POST['requestedFloor'];
+                $otherInfo = $_POST['otherInfo'];
+
+                $params = [
+                    'date' => $curr_date['CURRENT_DATE()'],
+                    'time' => $curr_time['CURRENT_TIME()'], 
+                    'status' => $status,
+                    'currentFloor' => $currentFloor,
+                    'requestedFloor' => $requestedFloor, 
+                    'otherInfo' => $otherInfo
+                ];
+                $result = $statement->execute($params);
+            }
+            
+            echo "<h3>Entire content of the elevatorNetwork table</h3>";
+            $query2 = 'SELECT * FROM elevatorNetwork GROUP BY nodeID ORDER BY nodeID'; 
+            $rows = $db->query($query2); 
+            echo "DATE  |   TIME    |   NODEID  |   STATUS  |   CURRENTFLOOR    |   REQUESTED FLOOR     |   OTHERINFO <br/>";
+            foreach ($rows as $row) { 
+                echo $row['date'] . " | " . $row['time'] . " | " . $row['nodeID'] . " | " . $row['status'] . " | " . $row['currentFloor'] 
+                                  . " | " . $row['requestedFloor'] . " | " . $row['otherInfo'];
+                echo '<br/>';
+            }       
+
+        } else {
+            // User not authenticated
+            echo 'You are not authenticated - go away!'; 
+        }
+
+    ?>
 
 
-  echo "Click to <a href='logout.php'>logout</a>";
-
-
-
-
-
-
-
-
-
-
-} else {
-  echo 'You are not authorized and must be logged in to see this content';
-}
-
-?>
+</body>
+</html>
--- php/updateDeleteEx.php
+++ php/updateDeleteEx.php
@@ -32,9 +32,8 @@ foreach ($rows as $row) {
 
 // Example 2 - function to update certain fields in a database
 /* *************************************************************** */
-/*
 
-function update_elevatorNetwork(int $node_ID, int $new_status = 1): void {
+function update_elevatorNetwork(string $tablename, int $node_ID, int $new_status = 1): void {
     $db = new PDO(
         'mysql:host=127.0.0.1;dbname=elevator',     // Database name
         'michaelG',                                 // username
@@ -47,8 +46,10 @@ function update_elevatorNetwork(int $node_ID, int $new_status = 1): void {
     $db->beginTransaction();                        // Roll back point
     try {
         // Change elevatorNetwork 
-        $query = "UPDATE elevatorNetwork SET status = :stat WHERE nodeID = :id"; 
+        $query = "UPDATE $tablename SET status = :stat WHERE nodeID = :id"; // This allows different tables to be selected but risks an SQL injection at the $tablename variable
+                                                                            // A solution to this problem is to use a switch statment: https://stackoverflow.com/questions/182287/can-php-pdo-statements-accept-the-table-or-column-name-as-parameter
         $statement = $db->prepare($query); 
+        //$statement->bindValue('tablename', $tablename);   // This will not work when using bindValue/prepared statements as it risks an SQL injection (which is what preprared statements try to avoid)
         $statement->bindValue('stat', $new_status); 
         $statement->bindValue('id', $node_ID);
         $statement->execute();
@@ -68,9 +69,9 @@ function update_elevatorNetwork(int $node_ID, int $new_status = 1): void {
 }
 
 // Start program
-update_elevatorNetwork(1, 21);       // Should succeed if nodeID = 1 does not have status = 21
+update_elevatorNetwork('elevatorNetwork',1, 56);       // Should succeed if nodeID = 1 does not have status = 21
 echo '<br/><br/>';
-update_elevatorNetwork(1000, 6);    // Should fail since thre is no nodeID == 1000
+//update_elevatorNetwork(1000, 6);    // Should fail since thre is no nodeID == 1000
 
 
 
@@ -91,12 +92,12 @@ foreach ($rows as $row) {
     var_dump($row);
     echo '<br/><br/>';
 }
-*/
 
 
-// Example 3 - function to update ANY field in ANY database
+
+// Example 3 - function to update ANY field in ANY table
 /* *************************************************************** */
-
+/*
 function update(string $tablename, string $wherefield, int $wherefieldValue, string $fieldname, int $newfieldvalue): void {
     $db = new PDO(
         'mysql:host=127.0.0.1;dbname=elevator',     // Database name
@@ -110,7 +111,7 @@ function update(string $tablename, string $wherefield, int $wherefieldValue, str
     // Use transactions 
     $db->beginTransaction();        // Roll back point
     try {
-        // Change any field in any table - requires DIRECTLY changing the query string (this cannot be done using prepared statements - it is not allowed)
+        // Change ANY field in ANY table - requires DIRECTLY changing the query string (this cannot be done using prepared statements - it is not allowed)
         $query = "UPDATE $tablename SET $fieldname = $newfieldvalue WHERE $wherefield = $wherefieldValue";   // The DOUBLE QUOTES here are NECESSARY since we use single quotes for the strings !!!
         $statement = $db->prepare($query); 
         $statement->execute();
@@ -151,6 +152,6 @@ foreach ($rows as $row) {
     var_dump($row);
     echo '<br/><br/>';
 }
-
+*/
 
 ?>
