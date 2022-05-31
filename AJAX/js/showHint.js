2cebe46ec48c7027c4aba3347809cec44e483b5e
michael.galle@gmail.com <michael.galle@gmail.com>
2022-05-31 16:12:15 -0400

    Updated files in AJAX folder to more clearly show AJAX use

--- AJAX/js/showHint.js
+++ AJAX/js/showHint.js
@@ -1,24 +1,24 @@
 function showHint(str) {
-  if(str.length == 0) {
-    document.getElementById('txtHint').innerHTML = ''; // Clear suggested text
-    return;
-  } else {
-    var xmlhttp = new XMLHttpRequest();     // Instantiate a XMLHttpRequest object
-    // Function to be executed when readyState changes (server response ready)
-    xmlhttp.onreadystatechange = function() {
-      if(this.readyState == 4 && this.status == 200) {
-        // readyState holds the status of the XMLHttpRequest (4 means finished request and server response is ready)
-        // status hold 200 for OK
-        var resp = this.responseText;   // Text string returned from server in 'echo' statement
-        hintArray = JSON.parse(resp);   // Convert text string to javascript array in JSON format
-        printHint();
-      }
-    };
-    xmlhttp.open("GET", "../php/gethint.php?q=" + str, true); // Open connection
-    xmlhttp.send();                                        // Send request
+    if(str.length == 0) {
+      document.getElementById('txtHint').innerHTML = ''; // Clear suggested text
+      return;
+    } else {
+      var xmlhttp = new XMLHttpRequest();     // Instantiate a XMLHttpRequest object
+      // Function to be executed when readyState changes (server response ready)
+      xmlhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+          // readyState holds the status of the XMLHttpRequest (4 means finished request and server response is ready)
+          // status hold 200 for OK
+          var resp = this.responseText;   // Text string returned from server in 'echo' statement
+          hintArray = JSON.parse(resp);   // THIS IS A GLOBAL VARIABLE/ARRAY SINCE VAR IS NOT DECLARED - Convert text string to javascript array in JSON format 
+          printHint();
+        }
+      };
+      xmlhttp.open("GET", "../php/gethint.php?q=" + str, true); // Open connection
+      xmlhttp.send();                                        // Send request
+    }
   }
-}
-
-function printHint() {
-  document.querySelector('#txtHint').innerHTML = '<p>' + hintArray[0] + '</p>' + '<p>' + hintArray[1] + '</p>' + '<br />' ;
-}
+  
+  function printHint() {
+    document.querySelector('#txtHint').innerHTML = '<p>' + hintArray[0] + '</p>' + '<p>' + hintArray[1] + '</p>' + '<br />' ;
+  }
--- AJAX/php/gethint.php
+++ AJAX/php/gethint.php
@@ -1,42 +1,14 @@
 <?php
 
-// Example from: https://www.w3schools.com/xml/ajax_php.asp
+// Example adapted from: https://www.w3schools.com/xml/ajax_php.asp
 
 // Array of companies
-$a[] = "Amazon";
-$a[] = "Amazing";
-$a[] = "Best Buy";
-$b[] = "Costco";
+$a = array("Amazon", "Apple", "Alcatel", "BestBuy", "Bravo", "Cambria", "Dell", "FaceBook", "Netflix", "Google");
 
 // get teh q parameter from URL
 $q = $_REQUEST["q"];
 
-
-/*
-$hint = "";
-
 // If $q is not empty string "" then lookup all matching substrings in the array
-if ($q !== "") {
-  $q = strtolower($q);    // Convert request to lower case for comparison
-  $len=strlen($q);
-  foreach($a as $name){
-    if(stristr($q, substr($name, 0, $len))) {
-      if($hint ==="") {     // First matching array/database element
-        $hint = $name;
-      } else {              // Additional matching elements in the array/database
-        $hint .= ", $name"; // Concatenate matching elements
-      }
-    }
-  }
-}
-// Output 'no suggestion' if no hint found or output matching vallues
-echo $hint === "" ? "no suggestion": $hint;
-
-*/
-
-// Array implementation
-// If $q is not empty string "" then lookup all matching substrings in the array
-
 $index = 0;  // index for hintArr which holds elements with matching substrings
 if ($q !== "") {
   $q = strtolower($q);    // Convert request to lower case for comparison
@@ -51,7 +23,7 @@ if ($q !== "") {
 }
 
 // string sent to function that handles the 'onreadystatechange' event
-// Use json_encode() to convert array to a string and send to function that handles the 'onreadystatechange' event
+// Use json_encode() to convert array to a (JSON encoded) string and send to function that handles the 'onreadystatechange' event
 echo $index === 0 ? "no suggestion": json_encode($hintArr);
 
 ?>
