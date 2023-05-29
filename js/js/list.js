4d452412882221d9851473ce5d75a7c2fea30166
Michael Galle <Mgalle@conestogac.on.ca>
2023-05-29 11:56:20 -0400

    Added content to DOM_Tree example

--- js/js/list.js
+++ js/js/list.js
@@ -1,19 +1,34 @@
-var nutel = document.getElementById('two');
+//let nutel = document.getElementById('two');
+let nutel = document.querySelector('#two');
 nutel.setAttribute('class', 'hot');
 
-var newel = document.createElement('li');
-var newText = document.createTextNode('Frozen Mangoes');
+let newel = document.createElement('li');
+let newText = document.createTextNode('Frozen Mangoes');
+newText.textContent = 'Frozen strawberries';
+newel.className = 'cold';
+newel.removeAttribute('class');
+newel.setAttribute('class', 'cold');
+
 newel.appendChild(newText);
 
-var position = document.querySelector('ul');
-var pos2 = document.getElementById('one');
+let position = document.querySelector('ul');
+let pos2 = document.getElementById('one');
 
 // Place at end of list
-//position.appendChild(newel)
+position.appendChild(newel)
 
 // Place at beginning of list
-position.insertBefore(newel, pos2);
+//position.insertBefore(newel, pos2);
 
+
+// Show all li elements
+let nutel2 = document.querySelectorAll('li');
+nutel2[0].setAttribute('class', 'cold');
+
+nutel2.forEach(element => {             // foreach in JS
+    console.log(element);
+});
+nutel2[4].id = 'five';   //setAttribute('id', 'five');
 
 
 
