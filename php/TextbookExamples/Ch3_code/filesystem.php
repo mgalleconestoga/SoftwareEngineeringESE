81d5b4ee1f4a2ba1d6f95ed972e8919ff9f0ffd8
Michael Galle <Mgalle@conestogac.on.ca>
2023-06-06 13:15:27 -0400

    Modified content in php/TextbookExampls/filesystem.php

--- php/TextbookExamples/Ch3_code/filesystem.php
+++ php/TextbookExamples/Ch3_code/filesystem.php
@@ -1,7 +1,8 @@
  <?php
 	/*
-	$booksJson = file_get_contents(__DIR__ . '/json/books.json'); 
-	$booksArray = json_decode($booksJson, true);
+	// Get data from JSON file
+	$booksJson = file_get_contents(__DIR__ . '/json/books.json');   // Save as string
+	$booksArray = json_decode($booksJson, true);					// Convert string to array of objects in PHP
 	foreach ($booksArray as $book) {
 		echo "Title: " . $book["title"] . ", Author: ". $book["author"] . "<br />";
 	} 
@@ -9,21 +10,25 @@
  ?>
  
 <?php
-	/*
-	$booksJson = file_get_contents(__DIR__ . '/json/books.json'); 
-	$booksArray = json_decode($booksJson, true);
+	
+	// Get data from JSON file
+	$booksJson = file_get_contents(__DIR__ . '/json/books.json');  // Save as string 
+	$booksArray = json_decode($booksJson, true);				   // Convert string to array of objects in PHP
 	
 	foreach ($booksArray as $key => $book) {
 		$booksArray[$key]["title"] = $book["title"] . " - This is a great book!"; // EDIT TITLES
 		// echo "New Title: " . $booksArray[$key]["title"] . "<br />";
 	} 
+
 	// Now save the changes back to the JSON file
-	$booksJsonNew = json_encode($booksArray, true);
-	file_put_contents(__DIR__ . '/json/booksNew.json', $booksJsonNew); 
-	*/
+	$booksJsonNew = json_encode($booksArray, true);					// Convert PHP array of objects to a string
+	file_put_contents(__DIR__ . '/json/booksNew.json', $booksJsonNew); 	// Write the stringto the file
+	
+	echo "Successfully wrote to file booksNew.json !"
  ?>
  
  <?php 
+ /*
 	// File I/O for CSV files 
 	//mkdir(__DIR__ . "/csv");   							// run once 
 	$my2DArray = [ [1,1,1], [2,2,2], [3,3,3], [4,4,4], [5,5,5] ];  
@@ -58,4 +63,5 @@
 		fputcsv($fpNew, $subArray); 		// format line as csv and write to location of file pointer
 	}
 	fclose($fpNew);
+*/
  ?>
